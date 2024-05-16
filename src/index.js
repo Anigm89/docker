bfbaa447c9eb693129c4648d3b3046c4ff1fb05b
Aniana <anigm89@gmail.com>
2024-05-16 15:42:21 +0200

    no actualiza

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const PORT = 3000
 
 require('./db')
 
+app.use('/', (req, res) =>{
+  res.send('hola mundo, estoy aprendiendo docker y NO funcionaaaaaaaaaaa')
+})
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
